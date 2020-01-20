af73dc2543f88224b8f3958295f86a2201d43b4a
José Henrique Ivanchechen <joseivanchechen@outlook.com>
2020-01-20 14:38:12 -0300

    att

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost:27017')
+//module.exports = mongoose.connect('mongodb://localhost:27017')
+module.exports = mongoose.connect('mongodb+srv://todo:2EOzPBCLmlfkmSGZ@aps-rl4e8.mongodb.net/test?retryWrites=true&w=majority')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from "react";
 
 import Grid from './components/grid/Grid';
 import Register from './components/register/Register';
+import api from './models/api';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+        }
+    }
+
+    fetchTasks = () => {
+        api.get('/api/task').then(response => {
+            this.setState({ data: response.data });
+        });
+    }
+
     render() {
         return (
             <div id='App'>
                 <h1>todo list</h1>
                 
-                <Register />
-                <Grid />
+                <Register fetchTasks={this.fetchTasks} />
+                <Grid data={this.state.data} fetchTasks={this.fetchTasks} />
             </div>
         )
     }
