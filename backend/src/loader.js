4646b419bdfc702816c912082b672f0cef4156f4
José Henrique Ivanchechen <joseivanchechen@outlook.com>
2020-01-23 16:27:01 -0300

    deploy

--- backend/src/loader.js
+++ backend/src/loader.js
@@ -3,4 +3,7 @@ const   path    = require('path');
 require('./database');
 require('./routes')(server);
 
-//module.exports = server
+server.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    console.log('Serving files:', path.join(__dirname+'/client/build/index.html'))
+});
--- frontend/src/models/api.js
+++ frontend/src/models/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 'http://localhost:3333' : '';
+//const BASE_URL = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 'http://localhost:3333' : '';
+const BASE_URL = '';
 
 const axios_api = axios.create({
     baseURL: BASE_URL,
